01a105543f10d81cd770ced0c00f19d29115e65a
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-01-27 09:32:55 +0100

    Closure to controller

---
+++ src/Http/FeedController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Spatie\Feed\Http;
+
+use Illuminate\Routing\Controller;
+use Spatie\Feed\Feed;
+
+class FeedController extends Controller
+{
+    public function feed()
+    {
+        $configuration = $this->getFeedConfiguration();
+
+        // Overwrite the relative feed url with the request's absolute url
+        $configuration['url'] = request()->url();
+
+        return (new Feed($configuration))->getFeedResponse();
+    }
+
+    protected function getFeedConfiguration(): array
+    {
+        $feeds = config('laravel-feed.feeds');
+
+        $feedIndex = (int) str_replace('spatieLaravelFeed', '', static::$router->currentRouteName());
+
+        return $feeds[$feedIndex] ?? abort(404);
+    }
+}
