51b12ca7d2fbf00262e8e7b29b6a5e7f4e20f51f
Freek Van der Herten <freek@spatie.be>
2021-05-31 09:11:15 +0200

    Update links.blade.php

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -1,3 +1,3 @@
 @foreach($feeds as $name => $feed)
-    <link rel="alternate" type="{{ \Spatie\Feed\Helpers\FeedContentType::forLink($feed['format']) }}" href="{{ route("feeds.{$name}") }}" title="{{ $feed['title'] }}">
+    <link rel="alternate" type="{{ \Spatie\Feed\Helpers\FeedContentType::forLink($feed['format'] ?? 'atom') }}" href="{{ route("feeds.{$name}") }}" title="{{ $feed['title'] }}">
 @endforeach
