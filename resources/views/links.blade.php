e70dd9521ce076051cc6a0c4a2b56ebf7ddac41c
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-15 14:47:34 +0800

    Delete resources/views directory

--- resources/views/links.blade.php
+++
@@ -1,3 +0,0 @@
-@foreach($feeds as $name => $feed)
-    <link rel="alternate" type="{{ \Spatie\Feed\Helpers\FeedContentType::forLink($feed['format'] ?? 'atom') }}" href="{{ route("feeds.{$name}") }}" title="{{ $feed['title'] }}">
-@endforeach
