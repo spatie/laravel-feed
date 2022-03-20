43d746fd40c7a9c9aa36b18a122ec867db076d75
Dries Vints <dries@vints.io>
2022-03-20 15:52:14 +0100

    Update feed.php

--- config/feed.php
+++ config/feed.php
@@ -8,7 +8,7 @@ return [
              * the items that should appear in the feed. For example:
              * [App\Model::class, 'getAllFeedItems']
              *
-             * You can also pass an argument to that method.  Note that their key must be the name of the parameter:
+             * You can also pass an argument to that method. Note that their key must be the name of the parameter:
              * [App\Model::class, 'getAllFeedItems', 'parameterName' => 'argument']
              */
             'items' => '',
@@ -23,14 +23,14 @@ return [
             'language' => 'en-US',
 
             /*
-             * The image to display for the feed.  For Atom feeds, this is displayed as
+             * The image to display for the feed. For Atom feeds, this is displayed as
              * a banner/logo; for RSS and JSON feeds, it's displayed as an icon.
              * An empty value omits the image attribute from the feed.
              */
             'image' => '',
 
             /*
-             * The format of the feed.  Acceptable values are 'rss', 'atom', or 'json'.
+             * The format of the feed. Acceptable values are 'rss', 'atom', or 'json'.
              */
             'format' => 'atom',
 
@@ -40,13 +40,13 @@ return [
             'view' => 'feed::atom',
 
             /*
-             * The mime type to be used in the <link> tag.  Set to an empty string to automatically
+             * The mime type to be used in the <link> tag. Set to an empty string to automatically
              * determine the correct value.
              */
             'type' => '',
 
             /*
-             * The content type for the feed response.  Set to an empty string to automatically
+             * The content type for the feed response. Set to an empty string to automatically
              * determine the correct value.
              */
             'contentType' => '',
