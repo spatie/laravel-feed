593630bde0b40746c0f93bd8f8c43ffc12652804
freekmurze <freekmurze@users.noreply.github.com>
2022-10-26 10:23:02 +0000

    Fix styling

--- src/Helpers/Cdata.php
+++ src/Helpers/Cdata.php
@@ -4,14 +4,14 @@ namespace Spatie\Feed\Helpers;
 
 class Cdata
 {
-    public static function out( string $data ): string
+    public static function out(string $data): string
     {
         // See https://www.w3.org/TR/REC-xml/#dt-cdsection
         $replace = [
             '<!CDATA[' => '', // CDATA cannot be nested.
             ']]>' => ']]&gt;', // CDEnd needs to be escaped.
         ];
-        return '<![CDATA[' . str_replace(array_keys($replace), array_values($replace), $data ) . ']]>';
-    }
 
+        return '<![CDATA[' . str_replace(array_keys($replace), array_values($replace), $data) . ']]>';
+    }
 }
