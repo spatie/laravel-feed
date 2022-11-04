09afe082ab06ef991dfe1af63c6a223001da3c4a
Muhammed Sari <mabdullahsari@hotmail.com>
2022-11-05 00:20:21 +0100

    Add .stubs.php (#195)

---
+++ .stubs.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Illuminate\Routing
+{
+    class Router
+    {
+        public function feeds(): void {}
+    }
+}
+
+namespace Illuminate\Support\Facades
+{
+    class Route
+    {
+        public static function feeds(): void {}
+    }
+}
